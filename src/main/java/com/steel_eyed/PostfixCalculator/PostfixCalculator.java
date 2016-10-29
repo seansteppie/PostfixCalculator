f7344fc39ae181c17224420fcc77df13bd2288f3
Sean Steppie <Sean.Steppie@steel-eyed.com>
2016-10-29 15:14:07 +0100

    Remove total & instead push the value onto the stack

--- src/main/java/com/steel_eyed/PostfixCalculator/PostfixCalculator.java
+++ src/main/java/com/steel_eyed/PostfixCalculator/PostfixCalculator.java
@@ -14,23 +14,17 @@ public class PostfixCalculator {
     }
 
     public long evaluate( String s ) {
-        long total = 0;
-        boolean first_run = true;
-        Stack numerics = new Stack();
+        Stack< Long > numerics = new Stack<>();
         for( String token : s.split( "\\s+" ) ) {
             if( token.matches( "\\d+" ) ) {
                 numerics.push( Long.parseLong( token ) );
                 continue;
             }
             // Must be an operator
-            if( first_run ) {
-                total = calc( ( long ) numerics.pop(), ( long ) numerics.pop(), token );
-                first_run = false;
-            } else {
-                total = calc( total, ( long ) numerics.pop(), token );
-            }
+            long total = calc( numerics.pop(), numerics.pop(), token );
+            numerics.push( total );
         }
-        return total;
+        return numerics.pop();
     }
 
     public long calc( long value1, long value2, String operator ) {
