72356d0c265e4e80b5bcbcc98e402d9c76ee2e2b
Sean Steppie <Sean.Steppie@steel-eyed.com>
2016-10-29 14:03:44 +0100

    Tests

---
+++ src/test/java/com/steel_eyed/PostfixCalcutor/Test/KataTests.java
@@ -0,0 +1,25 @@
+package com.steel_eyed.PostfixCalcutor.Test;
+
+import com.steel_eyed.PostfixCalculator.PostfixCalculator;
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * KataTests
+ * <p>
+ * @author Sean Steppie
+ * <p>
+ * @version 1.0.0        Date: 28/10/16     Initial Version
+ */
+public class KataTests {
+    /**
+     * Test It
+     */
+    @Test
+    public void test1() throws Exception {
+        PostfixCalculator eval = new PostfixCalculator();
+        assertEquals( 10, eval.evaluate( "2 3 9 4 / + *" ) );
+    }
+}
