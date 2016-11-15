fd77f1a8787cd45760654546bd199cceecd19ea0
Sean Steppie <Sean.Steppie@steel-eyed.com>
2016-11-15 17:17:43 +0000

    Rename test package

--- src/test/java/com/steel_eyed/Numeric/PostfixCalcutor/Test/PostfixCalculatorTests.java
+++ src/test/java/com/steel_eyed/Numeric/PostfixCalcutor/Test/PostfixCalculatorTests.java
@@ -1,8 +1,8 @@
-package com.steel_eyed.PostfixCalcutor.Test;
+package com.steel_eyed.Numeric.PostfixCalcutor.Test;
 
+import com.steel_eyed.Numeric.PostfixCalculator;
 import org.junit.Test;
 
-import static com.steel_eyed.Numeric.PostfixCalculator.evaluate;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -18,31 +18,31 @@ public class PostfixCalculatorTests {
      */
     @Test
     public void test1() throws Exception {
-        assertEquals( 10, evaluate( "2 3 9 4 / + *" ) );
+        assertEquals( 10, PostfixCalculator.evaluate( "2 3 9 4 / + *" ) );
     }
 
     @Test
     public void test2() throws Exception {
-        assertEquals( 70, evaluate( "25 45 +" ) );
+        assertEquals( 70, PostfixCalculator.evaluate( "25 45 +" ) );
     }
 
     @Test
     public void test3() throws Exception {
-        assertEquals( 3600, evaluate( "20 40 + 60 *" ) );
+        assertEquals( 3600, PostfixCalculator.evaluate( "20 40 + 60 *" ) );
     }
 
     @Test
     public void test4() throws Exception {
-        assertEquals( 2000, evaluate( "20 40 60 + *" ) );
+        assertEquals( 2000, PostfixCalculator.evaluate( "20 40 60 + *" ) );
     }
 
     @Test
     public void test5() throws Exception {
-        assertEquals( 1180, evaluate( "20 60 -1 + *" ) );
+        assertEquals( 1180, PostfixCalculator.evaluate( "20 60 -1 + *" ) );
     }
 
     @Test( expected = RuntimeException.class )
     public void test6() throws RuntimeException {
-        evaluate( "10 20 %" );
+        PostfixCalculator.evaluate( "10 20 %" );
     }
 }
