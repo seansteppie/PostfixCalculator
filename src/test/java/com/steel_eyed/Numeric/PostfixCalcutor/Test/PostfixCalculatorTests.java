360e82dc676720a0669e6ffd0064d0ee35d2c519
Sean Steppie <Sean.Steppie@steel-eyed.com>
2016-11-16 18:54:16 +0000

    Rename test methods

--- src/test/java/com/steel_eyed/Numeric/PostfixCalcutor/Test/PostfixCalculatorTests.java
+++ src/test/java/com/steel_eyed/Numeric/PostfixCalcutor/Test/PostfixCalculatorTests.java
@@ -17,32 +17,32 @@ public class PostfixCalculatorTests {
      * Test It
      */
     @Test
-    public void test1() throws Exception {
-        assertEquals( 10, PostfixCalculator.evaluate( "2 3 9 4 / + *" ) );
+    public void testArray() throws Exception {
+        assertEquals( 10, PostfixCalculator.evaluate( new String[] { "2", "3", "9", "4", "/", "+", "*" } ) );
     }
 
     @Test
-    public void test2() throws Exception {
+    public void testSimpleCase() throws Exception {
         assertEquals( 70, PostfixCalculator.evaluate( "25 45 +" ) );
     }
 
     @Test
-    public void test3() throws Exception {
+    public void testMixOperandsOperators() throws Exception {
         assertEquals( 3600, PostfixCalculator.evaluate( "20 40 + 60 *" ) );
     }
 
     @Test
-    public void test4() throws Exception {
+    public void testDifferentOperatorOrder() throws Exception {
         assertEquals( 2000, PostfixCalculator.evaluate( "20 40 60 + *" ) );
     }
 
     @Test
-    public void test5() throws Exception {
+    public void testNegativeNumber() throws Exception {
         assertEquals( 1180, PostfixCalculator.evaluate( "20 60 -1 + *" ) );
     }
 
     @Test( expected = RuntimeException.class )
-    public void test6() throws RuntimeException {
+    public void testInvalidOperator() throws RuntimeException {
         PostfixCalculator.evaluate( "10 20 %" );
     }
 
